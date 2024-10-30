5b197f978267436cd76b6a7cb8e87cb1c8d140c5
dunzae <dunzae99@gmail.com>
2024-10-31 03:02:46 +0900

    @Chore build 파일 수정

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,4 +1,5 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
 import dotenv from "dotenv";
 import { DefinePlugin } from 'webpack';
 
@@ -18,11 +19,21 @@ module.exports = {
                 test: /\.css?$/,
                 use: ['style-loader', 'css-loader', "postcss-loader"],
             },
-
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
         ],
     },
     resolve: {
         extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+        alias: {
+            '@pages': path.resolve(__dirname, 'src/pages'),
+            '@assets': path.resolve(__dirname, 'src/assets'),
+            '@constants': path.resolve(__dirname, 'src/constants'),
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@containers': path.resolve(__dirname, 'src/containers'),
+        }
     },
     plugins: [
         new HtmlWebpackPlugin({
