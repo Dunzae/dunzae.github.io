a9f0ceda8253a2112d65c9c9844a94c88afab951
dunzae <dunzae99@gmail.com>
2024-10-31 17:37:52 +0900

    @Design 탭메뉴 하단 마진 설정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ function HomePage() {
             <div className="px-[10px] py-[10px] min-h-dvh bg-[#f2f2f2] lg:px-[40px] lg:py-[40px]">
                 <div>
                     {/* 공지 사항, 업데이트 목록 */}
-                    <div className="px-[10px] py-[10px] box-border bg-[#f2f2f2] font-Titillium font-bold lg:flex lg:top-0 lg:px-0 lg:py-0 lg:static lg:mb-[20px]">
+                    <div className="px-[10px] py-[10px] mb-[10px] box-border bg-[#f2f2f2] font-Titillium font-bold lg:flex lg:static lg:mb-[20px]">
                         <TabMenuComponent />
                     </div>
                 </div>
