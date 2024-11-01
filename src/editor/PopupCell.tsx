598a8d884f0c351671e251d1777b8fa8dee65ec1
dunzae <dunzae99@gmail.com>
2024-11-02 05:37:55 +0900

    @Design 팝업셀과 팝업리스트 디자인 완성

---
+++ src/editor/PopupCell.tsx
@@ -0,0 +1,22 @@
+interface IPopupCell {
+    icon : string;
+    title : string;
+    body : string;
+    onClick : () => void;
+}
+
+function PopupCell({icon, title, body, onClick} : IPopupCell) {
+    return (
+        <div className="flex gap-2 ite cursor-pointer" onClick={onClick}>
+                <div className="w-[50px] h-[50px] flex-shrink-0 shadow-sm rounded-md border border-slate-200">
+                    <img className="w-full h-full" src={icon} />
+                </div>
+                <div className="flex flex-col gap-1 justify-center">
+                    <span className="font-NanumGothic text-[12px] ">{title}</span>
+                    <span className="font-NanumGothic text-[10px] opacity-80">{body}</span>
+                </div>
+            </div>
+    )
+}
+
+export default PopupCell;
