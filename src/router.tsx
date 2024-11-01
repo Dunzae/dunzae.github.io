5f293b9db30369f82f2ab85ee8a9b808bf081d32
dunzae <dunzae99@gmail.com>
2024-11-02 05:37:28 +0900

    @Feat 글쓰기 페이지 라우터 완료

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 import HomePage from '@pages/Home';
+import WritePage from '@pages/Write';
 
 const router = createBrowserRouter([
     {
         'path' : '/',
         element : <HomePage />
+    },
+    {
+        "path" : "/write",
+        element : <WritePage />
     }
 ])
 
