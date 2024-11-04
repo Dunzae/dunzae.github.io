7e2a5111bc04270098b033a0ad2b1d001d712eba
dunzae <dunzae99@gmail.com>
2024-11-05 08:04:46 +0900

    @Feat 모달 기본 코드 생성

---
+++ src/components/common/Modal.tsx
@@ -0,0 +1,11 @@
+function ModalComponent() {
+    return (
+        <div className="fixed top-0 left-0 bottom-0 right-0 bg-gray-100 z-[10000] opacity-50"
+            onClick={(e) => e.stopPropagation()}
+        >
+
+        </div>
+    )
+}
+
+export default ModalComponent;
