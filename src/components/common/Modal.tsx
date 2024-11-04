7d3654410cff0d122a09a0b836dbe78b2cd92c86
dunzae <dunzae99@gmail.com>
2024-11-05 08:11:09 +0900

    @Feat 리덕스 값에 따라 모달이 보여질 지를 정함

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,6 +1,10 @@
+import { useAppSelector } from "@slices/store";
+
 function ModalComponent() {
+    const hidden = useAppSelector(({ modal }) => modal.hidden);
+
     return (
-        <div className="fixed top-0 left-0 bottom-0 right-0 bg-gray-100 z-[10000] opacity-50"
+        <div className={`${hidden ? 'hidden' : 'fixed'} top-0 left-0 bottom-0 right-0 bg-gray-100 z-[10000] opacity-50`}
             onClick={(e) => e.stopPropagation()}
         >
 
