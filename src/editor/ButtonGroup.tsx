7cfa8937e3510e4ae35b192ba7cafccd189f60df
dunzae <dunzae99@gmail.com>
2024-11-06 21:10:08 +0900

    @Feat 단축키 버튼 눌렀을 경우 모달이 보여지도록 설정

--- src/editor/ButtonGroup.tsx
+++ src/editor/ButtonGroup.tsx
@@ -1,11 +1,21 @@
 import { VscOpenPreview } from "react-icons/vsc";
 import { FiUploadCloud } from "react-icons/fi";
 import { FaRegKeyboard } from "react-icons/fa6";
+import { useAppDispatch } from "@slices/store";
+import { setHidden } from "@slices/modal";
 
 function ButtonGroup() {
+    const dispatch = useAppDispatch();
+    const onShortCutClick = () => {
+        dispatch(setHidden(false));
+    }
+
     return (
         <>
-            <button className='hidden w-fit rounded-lg gap-2 justify-center items-center font-NanumGothic font-bold text-md bg-white shadow-lg p-2 lg:flex lg:w-full lg:justify-start lg:rounded-none'>
+            <button
+                onClick={onShortCutClick}
+                className='hidden w-fit rounded-lg gap-2 justify-center items-center font-NanumGothic font-bold text-md bg-white shadow-lg p-2 lg:flex lg:w-full lg:justify-start lg:rounded-none'
+            >
                 <span> <FaRegKeyboard /></span>
                 <span> 단축키 </span>
             </button>
