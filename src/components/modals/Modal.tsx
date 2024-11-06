473981dfd0f9a72753532c951fa46e4932fd939e
dunzae <dunzae99@gmail.com>
2024-11-06 22:04:00 +0900

    @Design Modal의 크기를 각 모달 컴포넌트에서 맞도록 설정 변경

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -1,6 +1,6 @@
 import { useAppDispatch, useAppSelector } from "@slices/store";
 import { setHidden } from "@slices/modal";
-import ExitIcon from "@assets/icons/exit.png";
+
 
 // Modal 컴포넌트
 import YoutubeComponent from "./Youtube";
@@ -9,11 +9,7 @@ import ShortCutComponent from "./ShortCut";
 function ModalComponent() {
     const dispatch = useAppDispatch();
     const hidden = useAppSelector(({ modal }) => modal.hidden);
-
-    const exitOnClick = () => {
-        dispatch(setHidden(true));
-    }
-
+    
     return (
         <div
             onClick={(e) => {
@@ -22,21 +18,10 @@ function ModalComponent() {
             className={`${hidden ? 'hidden' : 'fixed'} top-0 left-0 bottom-0 right-0 bg-gray-100 z-[10000] bg-opacity-50`}
         >
             <div className="w-full h-full flex items-center justify-center">
-                <div
-                    onClick={(e) => e.stopPropagation()}
-                    className="w-[80%] h-[80%] bg-white border border-gray-200 relative rounded-xl shadow-lg bg-[url('./assets/images/shortcutBackgournd.png')] bg-contain bg-no-repeat bg-[50%_50%]"
-                >
-                    <div
-                        onClick={exitOnClick}
-                        className="absolute top-0 right-0 cursor-pointer"
-                    >
-                        <img src={ExitIcon} className="w-20 h-20" />
-                    </div>
                     {/* <ShortCutComponent /> */}
                     <YoutubeComponent />
                 </div>
             </div>
-        </div>
     )
 }
 
