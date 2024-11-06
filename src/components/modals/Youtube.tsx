35183bd78566d93a04e78d39f25c16a740060c25
dunzae <dunzae99@gmail.com>
2024-11-06 22:05:40 +0900

    @Feat  Youtube 모달 완성

--- src/components/modals/Youtube.tsx
+++ src/components/modals/Youtube.tsx
@@ -1,37 +1,45 @@
 import { setHidden } from "@slices/modal";
 import { useAppDispatch, useAppSelector } from "@slices/store";
-import { ChangeEvent, KeyboardEvent, useState } from "react";
+import {  KeyboardEvent } from "react";
+
+import ExitIcon from "@assets/icons/exit.png";
 
 function YoutubeComponent() {
-    const [url, setUrl] = useState('');
     const dispatch = useAppDispatch();
-    const editor = useAppSelector(({editor}) => editor.editor)
-    
-    const onChange = (e : ChangeEvent<HTMLInputElement>) => {
-        setUrl(e.target.value);
+    const editor = useAppSelector(({ editor }) => editor.editor)
+    const editorWidth = useAppSelector(({editor}) => editor.width);
+
+    const exitOnClick = () => {
+        dispatch(setHidden(true));
     }
 
     const onKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
-        if(e.key === 'Enter') {
+        if (e.key === 'Enter') {
             dispatch(setHidden(true));
-            editor?.chain().focus().setYoutubeVideo({src : url}).run();
-            setUrl('');
+            editor?.chain().focus().setYoutubeVideo({ src: e.currentTarget.value, width : editorWidth - 32}).run();
         }
     }
 
     return (
-        <div className="flex flex-col h-full">
+        <div
+            onClick={(e) => e.stopPropagation()}
+            className="w-[400px] h-[400px] flex flex-col bg-white border border-gray-200 relative rounded-xl shadow-lg bg-[url('./assets/images/shortcutBackgournd.png')] bg-contain bg-no-repeat bg-[50%_50%]"
+        >
+            <div
+                onClick={exitOnClick}
+                className="absolute top-0 right-0 cursor-pointer"
+            >
+                <img src={ExitIcon} className="w-20 h-20" />
+            </div>
             <div className="font-NanumGothic text-3xl p-5 font-bold border-b border-b-slate-300">
                 유투브 주소 입력
             </div>
             <div className="flex w-full h-full items-end">
-                <input type="text" 
-                        value={url}
-                        onChange={onChange}
-                        onKeyDown={onKeyDown}
-                        className="outline-none w-full h-10 font-NanumGothic font-bold text-3xl px-5 py-8"
-                        placeholder="유투브 URL 주소를 입력해주세요"
-                    />
+                <input type="text"
+                    onKeyDown={onKeyDown}
+                    className="outline-none w-full h-10 font-NanumGothic font-bold text-xl px-5 py-8"
+                    placeholder="유투브 URL 주소를 입력해주세요"
+                />
             </div>
 
 
