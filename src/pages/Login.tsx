2233a2fc0cc17b2920c77689e6e0fc9b69577af9
dunzae <dunzae99@gmail.com>
2024-11-11 04:33:11 +0900

    @Feat 로그인 컨테이너와 페이지 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,43 +1,10 @@
-import LoginComponent, { InputsType } from "@components/Login";
-import { useCallback, useMemo, useState } from "react";
-import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
+import LoginContaienr from "@containers/Login"
 
-const idPattern = /\w{6}.+/
-const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?`~])[^  |\n]+$/gm
 
 function LoginPage() {
-    const [errorMessage, setErrorMessage] = useState("");
-    const { register, handleSubmit } = useForm<InputsType>();
-
-    const onSubmit: SubmitHandler<InputsType> = useCallback(async ({ id, password, remember }) => {
-        setErrorMessage("");
-
-    }, [])
-
-    const onSubmitError: SubmitErrorHandler<InputsType> = useCallback((errors) => {
-        if (errors.id && errors.id.message) {
-            setErrorMessage(errors.id.message);
-            return;
-        }
-
-        if (errors.password && errors.password.message) {
-            setErrorMessage(errors.password.message);
-            return;
-        }
-    }, []);
-
-    const memorizedHandleSubmit = useMemo(() => handleSubmit(onSubmit, onSubmitError), []);
 
     return (
-        <div>
-            <LoginComponent
-                onSubmit={memorizedHandleSubmit}
-                errorMessage={errorMessage}
-                register={register}
-                idPattern={idPattern}
-                passwordPattern={passwordPattern}
-            />
-        </div>
+        <LoginContaienr />
     )
 }
 
