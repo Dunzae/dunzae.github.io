45cab0815ee29bdd126254091cc0383b598e32f8
dunzae <dunzae99@gmail.com>
2024-11-11 04:34:10 +0900

    @Chore 엑세스토큰과 리프레쉬 토큰의 전역 변수를 위한 리덕스 슬라이스 추가

--- src/slices/store.ts
+++ src/slices/store.ts
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import auth from '@slices/auth';
 import menu from "@slices/menu";
 import modal from "@slices/modal";
 import editor from "@slices/editor"
 
 export const store = configureStore({
-  reducer: { menu, modal, editor },
+  reducer: { auth, menu, modal, editor },
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
@@ -13,5 +14,5 @@ export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
 
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>() 
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
