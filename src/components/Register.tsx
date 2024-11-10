9a2bede600e233904c16983c770cf2d769ccc64b
dunzae <dunzae99@gmail.com>
2024-11-11 04:48:17 +0900

    @Feat Register 페이지, 컴포넌트, 컨테이너 추가 완료

---
+++ src/components/Register.tsx
@@ -0,0 +1,154 @@
+import { Link } from "react-router-dom";
+import { UseFormRegister } from "react-hook-form";
+import { FaLock, FaRegUser } from "react-icons/fa6";
+import { AiOutlineMail } from "react-icons/ai"; 
+
+export type InputsType = {
+    id: string;
+    email : string;
+    password: string;
+    remember: boolean;
+}
+interface IProps {
+    onSubmit: (e: React.FormEvent<HTMLFormElement>) => Promise<void>,
+    errorMessage: string,
+    register: UseFormRegister<InputsType>,
+    idPattern: RegExp,
+    emailPattern: RegExp,
+    passwordPattern: RegExp,
+}
+
+function RegisterComponent({
+    onSubmit,
+    errorMessage,
+    register,
+    idPattern,
+    emailPattern,
+    passwordPattern
+}: IProps) {
+    return (
+        <div className="h-dvh flex justify-center items-center bg-[url('assets/images/background.png')] bg-no-repeat bg-cover">
+            <form onSubmit={onSubmit} className="max-w-[350px] border border-white border-opacity-30 backdrop-blur-md rounded-lg overflow-hidden flex-1 py-[10px]">
+                <div className="flex  pb-[10px] my-[10px] ">
+                    <div className=" flex justify-center py-[10px]">
+                        <span className="text-[30px] text-white font-bold">Login</span>
+                    </div>
+                </div>
+                <div className="flex  px-[20px] py-[10px] overflow-hidden">
+                    <div className="flex w-full px-[10px] py-[10px] justify-between items-center border-2 border-white border-opacity-50 rounded-2xl  h-full">
+                        <div className="flex w-full h-full mr-2">
+                            <input autoComplete="username" className="w-full h-full bg-transparent text-white placeholder:text-white outline-none border-none"
+                                placeholder="Username"
+                                {
+                                ...register("id", {
+                                    required: {
+                                        value: true,
+                                        message: "아이디를 입력해주세요."
+                                    },
+                                    pattern: {
+                                        value: idPattern,
+                                        message: "아이디는 6글자 이상이여야 합니다."
+                                    }
+                                })
+                                }
+                            />
+                        </div>
+                        <div className="flex h-full flex-1 text-white">
+                            <FaRegUser />
+                        </div>
+                    </div>
+                </div>
+                <div className="flex  px-[20px] py-[10px] overflow-hidden">
+                    <div className="flex w-full px-[10px] py-[10px] justify-between items-center border-2 border-white border-opacity-50 rounded-2xl  h-full">
+                        <div className="flex w-full h-full mr-2">
+                            <input autoComplete="email" className="w-full h-full bg-transparent text-white placeholder:text-white outline-none border-none"
+                                placeholder="Email"
+                                {
+                                ...register("email", {
+                                    required: {
+                                        value: true,
+                                        message: "이메일을 입력해주세요."
+                                    },
+                                    pattern: {
+                                        value: emailPattern,
+                                        message: "이메일 형식으로 입력해주세요."
+                                    }
+                                })
+                                }
+                            />
+                        </div>
+                        <div className="flex h-full flex-1 text-white">
+                            <AiOutlineMail />
+                        </div>
+                    </div>
+                </div>
+                <div className="flex px-[20px] py-[10px] overflow-hidden">
+                    <div className="flex w-full px-[10px] py-[10px] justify-between items-center border-2 border-white border-opacity-50 rounded-2xl  h-full">
+                        <div className="flex w-full h-full mr-2 ">
+                            <input type="password" autoComplete="current-password" className="w-full h-full bg-transparent text-white placeholder:text-white outline-none border-none"
+                                placeholder="Password"
+                                {
+                                ...register("password", {
+                                    required: {
+                                        value: true,
+                                        message: "비밀번호를 입력해주세요."
+                                    },
+                                    pattern: {
+                                        value: passwordPattern,
+                                        message: "비밀번호는 6글자 이상, 영소문자, 영대문자, 특수문자(~!@#$%^&*)가 포함되어야 합니다."
+                                    }
+                                })
+                                }
+                            />
+                        </div>
+                        <div className="flex h-full flex-1 text-white">
+                            <FaLock />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex justify-between w-full px-[20px] py-[10px]">
+                    <div className="flex w-full justify-between items-center ">
+                        <div className="flex w-full">
+                            <div className="mr-1 basis-0">
+                                <input id="remember"
+                                    type="checkbox"
+                                    className="cursor-pointer"
+                                    {...register("remember")}
+                                />
+                            </div>
+                            <span className="inline-block text-white flex-shrink-0">
+                                <label htmlFor="remember" className="cursor-pointer">Remember Me</label>
+                            </span>
+                        </div>
+                        <div className="flex basis-auto justify-end">
+                            <button type="button" className="text-white flex-shrink-0">Forgot password</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex w-full px-[20px] mb-[10px]">
+                    <div className="flex w-full py-2 justify-center rounded-3xl bg-white ">
+                        <button type="submit" className=" h-full">Register</button>
+                    </div>
+                </div>
+                <div className="flex w-full">
+                    <div className='flex w-full px-[20px] pb-[10px] text-white justify-center'>
+                        <span className="mr-2">Don't have account?</span>
+                        <button type="button" className="border-b-2 border-transparent hover:border-white">
+                            <Link to="/login">
+                                Login
+                            </Link>
+                        </button>
+                    </div>
+                </div>
+                <div className="w-full mb-[10px]">
+                    <div className="w-full text-red-400  flex justify-center text-center px-[20px] py-[10px]">
+                        {errorMessage !== "" && errorMessage}
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default RegisterComponent;
---
+++ src/containers/Register.tsx
@@ -0,0 +1,82 @@
+import { useCallback, useContext, useMemo, useState } from "react";
+import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
+import { Navigate } from "react-router-dom";
+import RegisterComponent from "@components/Register";
+import ax from "@utils/axios";
+import ServerError from "@utils/error";
+import { InputsType } from "@components/Register";
+import { useAppDispatch, useAppSelector } from "@slices/store";
+import { setAccessToken, setRefreshToken } from "@slices/auth";
+import { setLocalStorageItem } from "@utils/storage";
+
+const idPattern = /\w{6}.+/
+const emailPattern = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/
+const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?`~])[^  |\n]+$/gm
+
+function RegisterContainer() {
+    const dispatch = useAppDispatch();
+    const {accessToken, refreshToken} = useAppSelector(({auth}) => auth);
+    const [errorMessage, setErrorMessage] = useState("");
+    const { register, handleSubmit } = useForm<InputsType>();
+
+    const onSubmit: SubmitHandler<InputsType> = useCallback(async ({ id, email, password, remember }) => {
+        setErrorMessage("");
+
+        const result = await ax.postApi("/auth/signUp", { id, email, password })
+        if (result.error !== undefined) {
+            setErrorMessage(ServerError[result.error]);
+        } else {
+            dispatch(setAccessToken(result.data?.accessToken));
+            dispatch(setRefreshToken(result.data?.refreshToken));
+            if(remember) {
+                setLocalStorageItem("token", {
+                    accessToken: result.data?.accessToken,
+                    refreshToken: result.data?.refreshToken,
+                })
+            } else {
+                setLocalStorageItem("token", {
+                    accessToken: result.data?.accessToken,
+                    refreshToken: result.data?.refreshToken,
+                }, {
+                    exp : "1h"
+                })
+            }
+        }
+    }, [])
+
+    const onSubmitError: SubmitErrorHandler<InputsType> = useCallback((errors) => {
+        if (errors.id && errors.id.message) {
+            setErrorMessage(errors.id.message);
+            return;
+        }
+
+        if (errors.email && errors.email.message) {
+            setErrorMessage(errors.email.message);
+            return;
+        }
+
+        if (errors.password && errors.password.message) {
+            setErrorMessage(errors.password.message);
+            return;
+        }
+    }, []);
+
+    const memorizedHandleSubmit = useMemo(() => handleSubmit(onSubmit, onSubmitError), []);
+
+    if (accessToken && refreshToken)
+        return <Navigate to="/" />;
+    
+    return (
+        <RegisterComponent
+            onSubmit={memorizedHandleSubmit}
+            errorMessage={errorMessage}
+            register={register}
+            idPattern={idPattern}
+            emailPattern={emailPattern}
+            passwordPattern={passwordPattern}
+        />
+
+    )
+}
+
+export default RegisterContainer;
