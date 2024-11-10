b75a72beafa25e84d92d657fcaf892ce6377074d
dunzae <dunzae99@gmail.com>
2024-11-11 04:34:46 +0900

    @Feat axios에서 url과 data만을 입력받는 get과 post api 생성

---
+++ src/utils/axios.ts
@@ -0,0 +1,48 @@
+import axios, { AxiosInstance, isAxiosError } from "axios";
+
+const ax : AxiosInstance = axios.create({
+    baseURL : process.env.SERVER_URL,
+    headers : {
+        'Content-Type' : "application/x-www-form-urlencoded"
+    }
+})
+
+ax.getApi = async (url : string, data : any) => {
+    try {
+        const result = await ax.get(url, data)
+        return result.data;
+    } catch(e) {
+        if (isAxiosError(e)) {
+            if(e.response === undefined) {
+                return {
+                    error: "The server is down",
+                }
+            }
+            return e.response?.data;
+        }
+        return {
+            error: "Unknown error",
+        }
+    }
+}
+
+ax.postApi = async (url : string, data : any) => {
+    try {
+        const result = await ax.post(url, data)
+        return result.data;
+    } catch(e) {
+        if (isAxiosError(e)) {
+            if(e.response === undefined) {
+                return {
+                    error: "The server is down",
+                }
+            }
+            return e.response?.data;
+        }
+        return {
+            error: "Unknown error",
+        }
+    }
+}
+
+export default ax;
