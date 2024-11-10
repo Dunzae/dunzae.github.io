39e2699a93b53638539e58adce6b3bce87429f97
dunzae <dunzae99@gmail.com>
2024-11-11 04:34:55 +0900

    @Feat 서버 에러 상수 설정

---
+++ src/utils/error.ts
@@ -0,0 +1,14 @@
+type ServerErrorType = {
+    [key : string] : string
+}
+
+const ServerError : ServerErrorType= {
+    "The input is empty" : "입력값이 비었습니다.",
+    "The input is invalid" : "입력값이 유효하지 않습니다.",
+    "User does not exist" : "해당 사용자가 존재하지 않습니다.",
+    "Password is not correct" : "패스워드가 틀렸습니다.",
+    "Unknown Error" : "알 수 없는 오류입니다.",
+    "The server is down" : "서버가 작동중이 아닙니다."
+}
+
+export default ServerError;
