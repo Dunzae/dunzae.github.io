2d2fcd9fdcec73b3b29193b01947c8e0859ead69
dunzae <dunzae99@gmail.com>
2024-11-11 22:03:46 +0900

    @Feat modal에 실패 시 보여질 컴포넌트 생성

---
+++ src/components/modals/Fail.tsx
@@ -0,0 +1,27 @@
+import ExitIcon from "@assets/icons/exit.png";
+import { setHidden } from "@slices/modal";
+import { useAppDispatch } from "@slices/store";
+
+function FailComponent() {
+    const dispatch = useAppDispatch();
+    const exitOnClick = () => {
+        dispatch(setHidden(true));
+    }
+
+    return (
+        <div className="w-[600px] h-[600px] flex flex-col bg-white border border-gray-200 relative rounded-xl shadow-lg">
+            <div
+                className="absolute top-0 right-0 cursor-pointer flex items-center"
+            >
+                <div onClick={exitOnClick}>
+                    <img src={ExitIcon} className="w-20 h-20" />
+                </div>
+            </div>
+            <div className="font-NanumGothic text-3xl p-5 font-bold border-b border-b-slate-300 w-full h-full flex items-center justify-center">
+                실패하였습니다
+            </div>
+        </div>
+    )
+}
+
+export default FailComponent;
