86d9b647d0c074b00c6807a8caae4ea29dd96aa9
dunzae <dunzae99@gmail.com>
2024-11-12 22:42:33 +0900

    @Feat 처음 들어왔을때 로컬 스토리지에 있는 토큰을 서버로 보내어 로그인을 확인함

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,39 @@ import "./global.css";
 import { store } from './slices/store'
 import { Provider } from 'react-redux'
 import ModalComponent from "@components/modals/Modal";
+import { useEffect } from "react";
+import { getLocalStorageItem, removeLocalStorageItem, setLocalStorageItem } from "@utils/storage";
+import { setAccessToken, setRefreshToken } from "@slices/auth";
+import ax from "@utils/axios";
+import ServerError from "@utils/error";
 
 const App = () => {
+  useEffect(() => {
+    const token = getLocalStorageItem("token");
+    if (token !== undefined) {
+      const checkAndRefreshToken = async function() {
+        const { accessToken, refreshToken } = token;
+        const result = await ax.postApi("/auth/check", {
+          accessToken,
+          refreshToken
+        })
+  
+        if(!result.error && result.data?.accessToken && result.data?.refreshToken) {
+          setLocalStorageItem("token", { 
+            accessToken : result.data?.accessToken,
+            refreshToken,
+          })
+
+          store.dispatch(setAccessToken(result.data.accessToken));
+          store.dispatch(setRefreshToken(result.data.refreshToken));
+        } else if(result.error === "The token is invalid") {
+          removeLocalStorageItem("token");
+        } 
+      }
+      checkAndRefreshToken();
+    }
+  }, [])
+
   return (
     <Provider store={store}>
       <ModalComponent />
