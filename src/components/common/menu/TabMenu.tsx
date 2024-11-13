d23d46d40f646e4576c60873cf31545893abd901
dunzae <dunzae99@gmail.com>
2024-11-13 09:02:46 +0900

    @Design 탭메뉴에서 Recommended로 변경

--- src/components/common/menu/TabMenu.tsx
+++ src/components/common/menu/TabMenu.tsx
@@ -1,7 +1,7 @@
 function TabMenuComponent() {
     return (
         <div className="w-full bg-white border border-gray flex justify-center rounded-md lg:border-b-2 lg:block">
-            <span className="p-4 inline-block cursor-pointer">Announcement</span>
+            <span className="p-4 inline-block cursor-pointer">Recommended</span>
             <span className="p-4 inline-block cursor-pointer box-border text-gray opacity-40">Update</span>
         </div>
     )
