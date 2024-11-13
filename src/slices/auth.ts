3ded702bf76a90a853be3831500b5df8e50ab924
dunzae <dunzae99@gmail.com>
2024-11-13 18:54:26 +0900

    @Feat accessToken이 리덕스 상태값으로 저장된다면 axios 인스턴스 header에 Authorization 값을 부여

--- src/slices/auth.ts
+++ src/slices/auth.ts
@@ -1,24 +1,32 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import ax from '@utils/axios'
 
 export interface IAuth {
-    accessToken? : string,
-    refreshToken? : string
+  accessToken?: string,
+  refreshToken?: string
 }
 
 const initialState: IAuth = {
-    accessToken : undefined,
-    refreshToken : undefined,
+  accessToken: undefined,
+  refreshToken: undefined,
 }
 
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    setAccessToken : (state, action : PayloadAction<string | undefined>) => {
-        state.accessToken = action.payload
+    setAccessToken: (state, action: PayloadAction<string | undefined>) => {
+      state.accessToken = action.payload
+
+      if(action.payload !== undefined) {
+        ax.interceptors.request.use((config) => {
+          config.headers.Authorization = `Bearer ${action.payload}`;
+          return config;
+        });
+      }
     },
-    setRefreshToken : (state, action : PayloadAction<string | undefined>) => {
-        state.refreshToken = action.payload
+    setRefreshToken: (state, action: PayloadAction<string | undefined>) => {
+      state.refreshToken = action.payload
     }
   },
 })
