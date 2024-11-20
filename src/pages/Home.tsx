f789ce98d90a167033fb7bfb3856596faae1b6b8
dunzae <dunzae99@gmail.com>
2024-11-21 05:16:59 +0900

    @Feat 홈페이지에서 react-query를 사용해서 api 요청하도록 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,62 @@
 import PostPreviewComponent from "@components/post/Preview";
 import TabMenuComponent from "@components/common/menu/TabMenu";
 import LayoutComponent from "@components/common/Layout";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import ax from "@utils/axios";
+import { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+
+export interface FetchPostsProps {
+    pageParam: number
+}
+
+const limit = 3
+
+async function fetchPostsInfinite({ pageParam }: FetchPostsProps) {
+    const uri = `/post/getList?skip=${pageParam}&limit=${limit}`;
+    const result = await ax.get(uri);
+
+    return result;
+}
 
 function HomePage() {
+    const ref = useRef<HTMLDivElement>(null);
+    const { data, isError, hasNextPage, isLoading, fetchNextPage } = useInfiniteQuery({
+        queryKey: ['posts'],
+        queryFn: async ({ pageParam }) => {
+            const result = await fetchPostsInfinite({ pageParam });
+
+            if (result.status !== 200) throw new Error(result.data);
+            return result.data;
+        },
+        initialPageParam: 0,
+        getNextPageParam: (lastPage, allPages, lastPageParam) => lastPageParam + limit,
+    })
+
+    useEffect(() => {
+        const handleObserver = (entries: IntersectionObserverEntry[]) => {
+            const target = entries[0];
+            if (target.isIntersecting && hasNextPage && !isLoading) {
+                fetchNextPage();
+            }
+        };
+
+        const observer = new IntersectionObserver(handleObserver, {
+            threshold: 0,
+        });
+
+        if (ref.current !== null) {
+            observer.observe(ref.current);
+        }
+
+        return () => {
+            if (ref.current !== null) {
+                observer.unobserve(ref.current);
+            }
+            observer.disconnect();
+        };
+    }, [hasNextPage, isLoading, fetchNextPage]);
+
     return (
         <LayoutComponent>
             <div className="px-[10px] py-[10px] min-h-dvh bg-[#f2f2f2] lg:px-[40px] lg:py-[40px]">
@@ -13,11 +67,23 @@ function HomePage() {
                     </div>
                 </div>
                 <div >
-                    <PostPreviewComponent />
-                    <PostPreviewComponent />
-                    <PostPreviewComponent />
+                    {(!isError && data) && data.pages.map(page => page.posts.map((post: any) =>
+                        <Link to={`/view?id=${post.id}`} >
+                            <PostPreviewComponent
+                                profile={post.author}
+                                title={post.title}
+                                content={post.body}
+                                thumbnail={post.thumbnail}
+                                like={post.likeNum}
+                                createDate={post.createDate}
+                            />
+                        </Link>
+                    ))}
                 </div>
             </div>
+            <div
+                className="w-full h-[100px]" ref={ref}
+            />
         </LayoutComponent>
     )
 }
