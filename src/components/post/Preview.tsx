46a5d0705c6bc9c910fb7e5a86113314126fd3ab
dunzae <dunzae99@gmail.com>
2024-11-21 05:17:46 +0900

    @Feat 게시글 미리보기 컴포넌트에서 서버 API 요청으로 받은 데이터를 Props로 전달

--- src/components/post/Preview.tsx
+++ src/components/post/Preview.tsx
@@ -1,35 +1,44 @@
 import ProfileImage from "@assets/images/profile.jpg"
-import BackgroundImage from "@assets/images/background.png";
+import EmptyThumbnailImage from "@assets/images/emptyThumbnail.png";
 import { FcLike } from "react-icons/fc";
 import { IoIosMore } from "react-icons/io";
 
-
 interface IPostPreviewComponent {
-    reverse?: boolean
+    profile: string,
+    createDate: string,
+    title: string,
+    content: string,
+    thumbnail?: string,
+    like: string,
 }
 function PostPreviewComponent({
-    reverse,
+    profile,
+    createDate,
+    title,
+    content,
+    thumbnail,
+    like,
 }: IPostPreviewComponent) {
     return <div className="border-b-2 p-4 mb-4 border-[#E6E6E6] bg-white rounded-[20px]">
-        <div className={`flex flex-col ${reverse ? 'lg:flex-row-reverse' : 'lg:flex-row'} lg:max-h-[250px] overflow-y-hidden gap-4 mb-4 justify-between`}>
-            <div className="flex flex-col">
+        <div className={`flex flex-col lg:flex-row lg:max-h-[255px] overflow-y-hidden gap-4 mb-4 justify-between`}>
+            <div className="flex flex-grow flex-col">
                 <div className="flex flex-row items-center mb-2 lg:mb-4">
                     <span>
                         <img src={ProfileImage} className="w-[30px] h-[30px] rounded-[50%] mr-2" />
                     </span>
-                    <span className="text-[14px] text-[#292929] font-NanumGothic font-bold">프로필 이름</span>
+                    <span className="text-[14px] text-[#292929] font-NanumGothic font-bold">{profile}</span>
                     <span className="mx-2">·</span>
-                    <span className="flex items-center text-[12px] text-[#757575] font-NanumGothic font-bold">작성 날짜</span>
+                    <span className="flex items-center text-[12px] text-[#757575] font-NanumGothic font-bold">{createDate}</span>
                 </div>
-                <div className="lg:flex lg:flex-col lg:w-full lg:max-w-[600px]">
-                    <p className="text-[#191919] mb-2 text-[22px] font-Roboto font-bold">Your portfolio is stopping you from geting that job</p>
-                    <p className="text-[#292929] h-[100px] font-Poppins font-normal overflow-y-hidden">
-                        An intense way to learn about the process and practice your designs skills — My 1st hackathon Hackathons have been on my mind since I heard it was a good way to gain experience as a junior UX designer. As my portfolio...
+                <div className="lg:flex lg:flex-col lg:w-full lg:pr-[200px]">
+                    <p className="text-[#191919] mb-2 text-[22px] font-Roboto font-bold">{title}</p>
+                    <p className="text-[#292929] leading-6 font-Poppins font-normal overflow-y-hidden text-sm">
+                        {content}
                     </p>
                 </div>
             </div>
             <div className="w-full h-auto lg:basis-[250px] lg:h-[250px] lg:flex-shrink-0 lg:flex-grow-0">
-                <img className="w-full h-full object-cover" src={BackgroundImage} />
+                <img className="w-full h-full object-cover" src={thumbnail ? `${process.env.SERVER_URL}/images/thumbnails/${thumbnail}` : EmptyThumbnailImage} />
             </div>
         </div>
         <div className="flex justify-between mb-4">
@@ -39,10 +48,9 @@ function PostPreviewComponent({
                         <FcLike size={20} />
                     </div>
                     <div className="w-[20px] h-[20px] flex font-Poppins font-bold relative">
-                        12
+                        {like}
                     </div>
                 </div>
-                <span className="p-3 mr-3 rounded-[20%] color-[#292929] border border-slate-200 font-normal font-Titillium shadow-2xl cursor-pointer">Category</span>
                 <span className="p-3 rounded-[20%] color-[#757575] border border-slate-200 font-normal font-Titillium shadow-2xl">3 min read</span>
             </div>
             <div className="flex items-center flex-grow-0 basis-0 cursor-pointer">
