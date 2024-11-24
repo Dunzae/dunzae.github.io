aef4465df4560b79ca60e76e17e47c21935d94a2
dunzae <dunzae99@gmail.com>
2024-11-24 10:56:00 +0900

    @Design 게시글 읽기에 대한 기초 디자인 설정

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -18,30 +18,35 @@ function Post({
     console.log(body);
 
     return (
-        <>
-            <div className="w-full h-[200px] relative mb-4">
-                {thumbnail !== undefined && <img src={`${process.env.SERVER_URL}/images/thumbnails/${thumbnail}`} className="w-full h-full object-cover absolute" />}
+        <div className="p-4 h-full flex flex-col">
+            <div className="w-full relative mb-8 h-[100px] lg:h-[200px] shadow-xl lg:border lg:border-gray-300">
+                {thumbnail !== undefined && <img src={`${process.env.SERVER_URL}/images/thumbnails/${thumbnail}`} className="hidden lg:block w-full h-full object-cover absolute" />}
                 <div className=" w-full h-full flex justify-center items-center absolute">
-                    <div className="text-white bg-black p-3 rounded-xl text-[40px] font-bold font-NanumGothic">
+                    <div className="text-white text-center bg-black p-3 rounded-xl text-[40px] font-bold font-NanumGothic">
                         {title}
                     </div>
                 </div>
             </div>
-            <div className="px-5" dangerouslySetInnerHTML={{
-                __html: body 
-            }}>
+            <div className="p-5 text-white shadow-xl bg-[#1f1f1f] rounded-xl flex-grow" dangerouslySetInnerHTML={{
+                __html: body
+            }} />
 
+
+            <div className="flex justify-between">
+                <div>
+                    좋아요 수 : {likeNum}
+                </div>
+                <div>
+                    <div>
+                        작성자 : {author}
+                    </div>
+
+                    <div>
+                        {createDate}
+                    </div>
+                </div>
             </div>
-            <div>
-                {author}
-            </div>
-            <div>
-                {likeNum}
-            </div>
-            <div>
-                {createDate}
-            </div>
-        </>
+        </div>
     )
 }
 
