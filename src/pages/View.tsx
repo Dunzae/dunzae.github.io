d9345ed6640124332a134b38f31340de61cf1294
dunzae <dunzae99@gmail.com>
2024-11-24 10:56:23 +0900

    @Feat 포스트 읽기를 위한 reactQuery 기능 완성

--- src/pages/View.tsx
+++ src/pages/View.tsx
@@ -16,24 +16,29 @@ async function fetchPost({ id }: FetchPostProps) {
 }
 
 function ViewPage() {
-    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams] = useSearchParams();
     const { data, isError, isLoading } = useQuery({
         queryKey: ["post"],
         queryFn: async () => {
-            const id = searchParams.get("id") ?? "";
-            const result = await fetchPost({ id });
+            const id = searchParams.get("id");
+            if(id === null) throw new Error("id is null");
 
+            const result = await fetchPost({ id });
             if (result.status !== 200) throw new Error(result.data);
             return result.data;
         }
     })
 
     if (isError) {
-        return <div>Error</div>
+        return <div className="flex items-center justify-center">
+            <p>서버에서 에러가 발생했습니다.</p>
+            <p>다시 실행해주세요!</p>
+        </div>
     }
 
     return (
         <LayoutComponent>
+            {isLoading && <div>로딩중</div>}
             {!isLoading && data && <Post
                 author={data.author}
                 title={data.title}
