cda2d8af99169e85cfa4b10ccfb00090192315d0
dunzae <dunzae99@gmail.com>
2024-11-25 20:26:48 +0900

    @Feat 댓글 기능 완성

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,5 +1,10 @@
 import "./style.scss";
 import Comment from "./Comment"
+import ax from "@utils/axios";
+import { useState } from "react";
+import { setHidden, setType } from "@slices/modal";
+import { useAppDispatch } from "@slices/store";
+import { useLocation } from "react-router-dom";
 
 interface PostProps {
     author: string
@@ -24,6 +29,26 @@ function Post({
     thumbnail,
     createDate
 }: PostProps) {
+    const dispatch = useAppDispatch();
+    const location = useLocation();
+    const params = new URLSearchParams(location.search);
+    const [comment, setComment] = useState("");
+
+    const onCommentSubmit = async () => {
+        const result = await ax.postApi("/post/comment/write", {
+            content: comment,
+            postId : params.get("id")
+        });
+
+        if (result.error !== undefined) {
+            dispatch(setType("Fail"));
+            dispatch(setHidden(false));
+        } else {
+            dispatch(setType("Success"));
+            dispatch(setHidden(false));
+        }
+    }
+
     return (
         <div className="p-4 w-full h-full flex flex-col gap-4 tiptap">
             <div className="w-full p-6 flex items-center justify-center mb-4 lg:border lg:border-gray-300 flex-shrink-0 bg-white border border-neutral-200 rounded-xl relative">
@@ -37,8 +62,8 @@ function Post({
             }} />
 
             <div>
-                <h3 className="pl-1 text-xl font-bold font-NanumGothic mb-4">댓글</h3>
-                <div className="flex flex-col gap-4 pb-10">
+                <h3 className="pl-1 pb-1 text-xl font-bold font-NanumGothic border-b border-b-gray-300 mb-4">댓글</h3>
+                {comments.length !== 0 && <div className="flex flex-col gap-4 pb-10">
                     {
                         comments.map((comment, index) =>
                             <Comment
@@ -50,8 +75,14 @@ function Post({
 
                         )
                     }
+                </div>}
+                <div>
+                    <h4 className="text-lg font-bold font-NanumGothic mb-2">댓글 달기</h4>
+                    <div className="w-full flex gap-4">
+                        <input value={comment} onChange={(e) => setComment(e.target.value)} type="text" className="border border-b-black p-2 flex-grow outline-none" placeholder="댓글을 입력해주세요" />
+                        <button className="bg-black text-white rounded-lg p-2" onClick={onCommentSubmit}>작성 완료</button>
+                    </div>
                 </div>
-                
             </div>
         </div>
     )
