5f6c618a2410beb93c50399e1da39318f8a179b4
dunzae <dunzae99@gmail.com>
2024-11-26 10:21:26 +0900

    @Feat 타이틀 눌렀을때 홈페이지로 이동

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 interface ITitleComponent {
     textColor?: string;
 }
@@ -9,7 +10,9 @@ function TitleComponent({
         <div style={{ color: textColor }} className="w-full text-center flex-shirink-0 h-auto border-b border-b-gray-200 lg:border-b-0">
             {/* 블로그 이름 */}
             <div className="text-[60px] font-[Agdasima] font-light ">
-                LogSomething
+                <Link to="/">
+                    LogSomething
+                </Link>
             </div>
 
         </div>
