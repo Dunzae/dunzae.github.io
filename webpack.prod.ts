63c5489f8f245cd05fec29ff5b3b4b92e46e60b2
dunzae <dunzae99@gmail.com>
2024-11-27 15:37:24 +0900

    @Chore 빌드 시 웹팩이 webp 파일을 인식할 수 있도록 설정

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -31,7 +31,7 @@ module.exports = {
                 ],
             },
             {
-                test: /\.(png|jpg|jpeg|gif)$/i,
+                test: /\.(png|jpg|jpeg|gif|webp)$/i,
                 type: "asset/resource",
             },
         ],
